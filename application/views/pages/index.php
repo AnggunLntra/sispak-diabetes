df7c79afc9c07b77d95965401052e288badb29bb
Anggun Lenteraningati <anggunlentera@gmail.com>
2023-04-11 18:36:01 +0700

    add controllers index, folder pages, and assets

---
+++ application/views/pages/index.php
@@ -0,0 +1,14 @@
+<div class="container pt-5">
+    <div class="row flex-wrap-reverse">
+        <div class="col-lg-6 d-flex pt-sm-5">
+            <div class="align-self-center">
+                <h1 class="m-0">Sistem Pakar Diagnosis <span class="text-danger">Diabetes Melitus</span></h1>
+                <h5 class="fw-light fst-italic">Berdasarkan Gejala</h5>
+                <a href="#" class="btn btn-danger mt-3">Mulai Tes</a>
+            </div>
+        </div>
+        <div class="col-lg-6 py-lg-5">
+            <img src="<?php echo base_url() ?>assets/img/img-diabetes-symptoms.jpg" alt="" class="img-fluid w-100">
+        </div>
+    </div>
+</div>
