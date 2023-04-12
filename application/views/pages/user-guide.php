bbedf5214cc3ca60dc923e7399d2a2084a73c73e
Anggun Lenteraningati <anggunlentera@gmail.com>
2023-04-12 21:42:45 +0700

    modified user guide

---
+++ application/views/pages/user-guide.php
@@ -0,0 +1,41 @@
+<div class="container min-vh-100" style="margin-top: 8rem;">
+    <h1 class="fw-bold text-center my-5">Cara Penggunaan <span class="text-uppercase text-danger">sipakar</span> :</h1>
+    <div class="row">
+        <div class="col-lg-4 mb-3">
+            <div class="card p-3">
+                <div class="card-title d-flex justify-content-center py-3">
+                    <div class="d-flex justify-content-center align-items-center rounded-circle bg-body-secondary" style="width: 50px; height: 50px">
+                        <span class="fw-bolder">1</span>
+                    </div>
+                </div>
+                <div class="body text-center">
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sed nobis a cum doloremque recusandae ex, voluptatum ab mollitia facilis unde? Corporis ipsum doloremque illo optio?</p>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-4 mb-3">
+            <div class="card p-3">
+                <div class="card-title d-flex justify-content-center py-3">
+                    <div class="d-flex justify-content-center align-items-center rounded-circle bg-danger-subtle" style="width: 50px; height: 50px">
+                        <span class="fw-bolder">2</span>
+                    </div>
+                </div>
+                <div class="body text-center">
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sed nobis a cum doloremque recusandae ex, voluptatum ab mollitia facilis unde? Corporis ipsum doloremque illo optio?</p>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-4 mb-3">
+            <div class="card p-3">
+                <div class="card-title d-flex justify-content-center py-3">
+                    <div class="d-flex justify-content-center align-items-center rounded-circle bg-danger" style="width: 50px; height: 50px">
+                        <span class="fw-bolder text-light">3</span>
+                    </div>
+                </div>
+                <div class="body text-center">
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sed nobis a cum doloremque recusandae ex, voluptatum ab mollitia facilis unde? Corporis ipsum doloremque illo optio?</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
