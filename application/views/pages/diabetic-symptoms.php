2778680b99aeeca8a2f312b778f8e1925b6fcea6
Anggun Lenteraningati <anggunlentera@gmail.com>
2023-04-12 21:08:44 +0700

    modified page diabetic symptoms and diabetic types page

--- application/views/pages/diabetic-symptoms.php
+++ application/views/pages/diabetic-symptoms.php
@@ -2,58 +2,58 @@
     <h1 class="fw-bold text-center my-5">Gejala-Gejala Diabetes</h1>
     <div class="row">
         <div class="col-6 col-lg-3 mb-3">
-            <div class="card bg-secondary py-3">
+            <div class="card bg-secondary py-3 shadow">
                 <div class="card-body text-center">
-                    <span class="fs-5 fw-bold">Gejala</span>
+                    <span class="fs-6 fw-bold text-light">Gejala</span>
                 </div>
             </div>
         </div>
         <div class="col-6 col-lg-3 mb-3">
-            <div class="card bg-danger py-3">
+            <div class="card bg-danger py-3 shadow">
                 <div class="card-body text-center">
-                    <span class="fs-5 fw-bold">Gejala</span>
+                    <span class="fs-6 fw-bold text-light">Gejala</span>
                 </div>
             </div>
         </div>
         <div class="col-6 col-lg-3 mb-3">
-            <div class="card bg-light py-3">
+            <div class="card bg-light py-3 shadow">
                 <div class="card-body text-center">
-                    <span class="fs-5 fw-bold">Gejala</span>
+                    <span class="fs-6 fw-bold">Gejala</span>
                 </div>
             </div>
         </div>
         <div class="col-6 col-lg-3 mb-3">
-            <div class="card bg-dark py-3">
+            <div class="card bg-dark py-3 shadow">
                 <div class="card-body text-center">
-                    <span class="fs-5 fw-bold text-light">Gejala</span>
+                    <span class="fs-6 fw-bold text-light">Gejala</span>
                 </div>
             </div>
         </div>
         <div class="col-6 col-lg-3 mb-3">
-            <div class="card bg-dark py-3">
+            <div class="card bg-dark py-3 shadow">
                 <div class="card-body text-center">
-                    <span class="fs-5 fw-bold text-light">Gejala</span>
+                    <span class="fs-6 fw-bold text-light">Gejala</span>
                 </div>
             </div>
         </div>
         <div class="col-6 col-lg-3 mb-3">
-            <div class="card bg-light py-3">
+            <div class="card bg-light py-3 shadow">
                 <div class="card-body text-center">
-                    <span class="fs-5 fw-bold">Gejala</span>
+                    <span class="fs-6 fw-bold">Gejala</span>
                 </div>
             </div>
         </div>
         <div class="col-6 col-lg-3 mb-3">
-            <div class="card bg-danger py-3">
+            <div class="card bg-danger py-3 shadow">
                 <div class="card-body text-center">
-                    <span class="fs-5 fw-bold">Gejala</span>
+                    <span class="fs-6 fw-bold text-light">Gejala</span>
                 </div>
             </div>
         </div>
         <div class="col-6 col-lg-3 mb-3">
-            <div class="card bg-secondary py-3">
+            <div class="card bg-secondary py-3 shadow">
                 <div class="card-body text-center">
-                    <span class="fs-5 fw-bold">Gejala</span>
+                    <span class="fs-6 fw-bold text-light">Gejala</span>
                 </div>
             </div>
         </div>
