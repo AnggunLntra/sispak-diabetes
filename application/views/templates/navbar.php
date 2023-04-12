fd4608997086974fb34370b8d107c328834ca4cc
Anggun Lenteraningati <anggunlentera@gmail.com>
2023-04-12 15:58:13 +0700

    modified navbar : add link menu beranda

--- application/views/templates/navbar.php
+++ application/views/templates/navbar.php
@@ -8,7 +8,7 @@
             <div class="collapse navbar-collapse" id="navbarCollapse">
                 <ul class="navbar-nav me-auto mb-2 mb-md-0">
                     <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="#">Beranda</a>
+                        <a class="nav-link active" aria-current="page" href="<?php echo base_url() ?>">Beranda</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" href="<?php echo base_url('pages/test_page') ?>">Diagnosis Diabetes</a>
