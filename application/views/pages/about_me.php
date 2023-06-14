b5989765a87f70151f23930e338ddc5207ad9ff6
Anggun Lenteraningati <anggunlentera@gmail.com>
2023-06-14 21:51:13 +0700

    add views about me (user)

---
+++ application/views/pages/about_me.php
@@ -0,0 +1,50 @@
+<div class="container min-vh-100" style="margin-top: 8rem;">
+    <div class="row">
+        <div class="col-12 card bg-light border-0 mb-3 py-4">
+            <div class="card-title text-center">
+                <span class="fw-bold fs-2">Tentang <span class="text-uppercase text-danger">sipakar</span></span>
+            </div>
+            <div class="card-body">
+                <p>Sipakar adalah sistem pakar berbasis website yang dibuat dengan tujuan untuk memudahkan masyarakat dalam melakukan diagnosis secara dini penyakit yang diderita. Untuk saat ini sipakar hanya dapat melakukan diagnosis secara dini penyakit diabetes melitus berdasarkan gejala-gejala yang biasa dialami oleh kebanyakan penderita penyakit diabetes.</p>
+                <p>Terdapat beberapa menu yang ada didalam website <span class="text-uppercase text-danger fw-bold">sipakar</span> yaitu :</p>
+                <div class="row">
+                    <div class="col-6 col-lg-3 mb-3 mb-lg-0">
+                        <div class="card bg-danger py-2 text-center shadow">
+                            <div class="card-body text-white text-center">
+                                <span class="fw-bold">Tes Diagnosis Diabetes</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-6 col-lg-3 mb-3 mb-lg-0">
+                        <div class="card bg-secondary py-2 text-center shadow">
+                            <div class="card-body">
+                                <span class="fw-bold">Gejala Diabetes</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-6 col-lg-3 mb-3 mb-lg-0">
+                        <div class="card bg-danger py-2 text-center shadow">
+                            <div class="card-body text-white">
+                                <span class="fw-bold">Tipe Diabetes</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-6 col-lg-3 mb-3 mb-lg-0">
+                        <div class="card bg-secondary py-2 text-center shadow">
+                            <div class="card-body">
+                                <span class="fw-bold">Cara Penggunaan</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-12 card border-0">
+            <div class="card-title text-center">
+            </div>
+            <div class="card-body">
+
+            </div>
+        </div>
+    </div>
+</div>
